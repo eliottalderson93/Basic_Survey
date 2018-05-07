dd6d53d78ec9972222ab4b3591e32ca386f5f2d3
Erik Nordland <eriknordland@gmail.com>
2018-05-07 11:11:40 -0700

    added validation 5/7

--- server.py
+++ server.py
@@ -1,21 +1,32 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
+import re
 app = Flask(__name__)
+app.secret_key = 'KeepItSecretKeepItSafe'
 # our index route will handle rendering our form
-@app.route('/')
+@app.route('/',methods = ['GET'])
 def index():
     return render_template("survey.html")
 # this route will handle our form submission
 # notice how we defined which HTTP methods are allowed by this route
 @app.route('/submitted', methods=['POST']) #
 def create_user():
-    print("Got Post Info")
-    # # we'll talk about the following two lines after we learn a little more about forms
-    name = request.form['name']
-    location = request.form['location']
-    lang = request.form['coding_language']
-    comment = request.form['comment']
-    # # redirects back to the '/' route
-    return render_template("code.html",name=name,location=location,lang=lang,comment=comment)
+    #print("Got Post Info")
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['lang'] = request.form['coding_language']
+    session['comment'] = request.form['comment']
+    if len(session['name'])<1:
+        flash('empty name')
+        return redirect('/')
+    elif len(session['comment'])<1:
+        flash('empty comment')
+        return redirect('/')
+    elif len(session['comment'])>120:
+        flash('comment too long, shorten to 120 characters')
+        return redirect('/')
+    else:
+        flash('successfully submitted form!')
+        return render_template("code.html")
 @app.route('/danger')
 def back():
     print("STAY OUT")
